618986c39872d1522d805e52234a5984436a292d
Edward Withers <dearshrewdwit@gmail.com>
2020-10-28 12:26:26 +0000

    remove files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 window.addEventListener("load", (event) => {
   let appElement = document.getElementById("app");
-  new Headlines({element: appElement, client: GuardianClient()})
+  let headlines = new Headlines({element: appElement, client: GuardianClient()})
 
   window.addEventListener("hashchange", (event) => {
-    let url = location.hash.split('summaries/')[1]
     appElement.innerHTML = ''
-    new ArticleSummary({element: appElement, client: AylienClient(), url: url})
+    let id = location.hash.split('summaries/')[1]
+    let item = headlines.findBy(id)
+    new ArticleSummary({element: appElement, aylienClient: AylienClient(), guardianClient: GuardianClient(), item: item})
   });
 })
